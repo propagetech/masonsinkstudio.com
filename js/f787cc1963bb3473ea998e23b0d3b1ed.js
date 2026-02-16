21c28d8c663437b46fcca3cd1a260c987c70c7cc
Propagatech <propagetech@gmail.com>
2026-02-16 22:37:21 +0530

    Update index.html and CSS files to enhance SEO and replace external resources with local assets. Added new images and SVG icons, and improved font handling in stylesheets.

--- js/f787cc1963bb3473ea998e23b0d3b1ed.js
+++ js/f787cc1963bb3473ea998e23b0d3b1ed.js
@@ -9,7 +9,7 @@ window.botpressWebChat.init({
     "lazySocket": true,
     "themeName": "prism",
     "botName": "Chat with Us",
-    "avatarUrl": "https://5d08fafa95adde44b7e0-3224566938adbe13b75c7275783f1972.ssl.cf1.rackcdn.com/1454566371794Masonsink.png",
+    "avatarUrl": "imgs/6a350c60bc614268bf0c0ed460faff6c.png",
     "stylesheet": "https://webchat-styler-css.botpress.app/prod/code/b755a24c-55d0-4741-8696-8de65d4ca05c/v23348/style.css",
     "frontendVersion": "v1",
     "useSessionStorage": true,
